687b88b2885c083a861d096cfcde9850187a7d2a
GuanYen <guanyen011003@gmail.com>
2024-10-28 20:12:25 -0500

    added myboot2() function

--- R/myclt.R
+++ R/myclt.R
@@ -10,7 +10,7 @@
 #' \dontrun{w=myclt(n=10,iter=10000)}
 myclt=function(n,iter){
   y=runif(n*iter,0,5) # A
-  data=matrix(y,nr=n,nc=iter,byrow=TRUE) #B
+  data=matrix(y,nrow=n,ncol=iter,byrow=TRUE) #B
   sm=apply(data,2,sum) #C
   hist(sm)
   sm
