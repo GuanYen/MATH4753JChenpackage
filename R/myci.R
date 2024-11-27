06914a3822843310a96940e0d706a021cdfd6087
GuanYen <guanyen011003@gmail.com>
2024-11-26 21:05:08 -0600

    syntax error

--- R/myci.R
+++ R/myci.R
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-#'   \dontrun{myci(x=c(5.0581, 4.9707, 5.0893, 4.9334, 4.9777, 5.0285, 4.8555, 4.9565)}
+#'   \dontrun{myci(x=c(5.0581, 4.9707, 5.0893, 4.9334, 4.9777, 5.0285, 4.8555, 4.9565))}
 myci<-function(x,ci=0.95){
   t.test(x,conf.level=ci)$conf.int
 }
